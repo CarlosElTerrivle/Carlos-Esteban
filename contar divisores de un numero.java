95ae4d429b63dafc08e736e9527e38d9c2ed543d
CarlosElTerrivle <48688160+CarlosElTerrivle@users.noreply.github.com>
2019-04-03 12:29:29 -0300

    Update contar divisores de un numero.java

--- contar divisores de un numero.java
+++ contar divisores de un numero.java
@@ -9,21 +9,16 @@ public class Asdf {
         System.out.println("ingrese un numero");
         int num = in.nextInt();
         int num2 = 0;
+        int num3 = 0;
         for (int i = 1; i <= num; i++) {
             if (num % i == 0) {
                 System.out.println("divisor: " + i);
                 num2++;
+
             }
         }
-/**       if (num2 == 2) {
-            System.out.println("es numero primo");
 
-        } else {
-            if (!(num2 == 2)) {
-                System.out.println("el numero no es primo");
-/*         }
-        }
-
+        System.out.println("los divisores son: " + num2);
     }
-
 }
+
