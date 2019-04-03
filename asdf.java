5ef99ed0eae136cf92c38edb3122ecca98d701f7
CarlosElTerrivle <48688160+CarlosElTerrivle@users.noreply.github.com>
2019-04-03 11:55:09 -0300

    Create asdf.java

---
+++ asdf.java
@@ -0,0 +1,29 @@
+package asdf;
+
+import java.util.Scanner;
+
+public class Asdf {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("ingrese un numero");
+        int num = in.nextInt();
+        int num2 = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+//                System.out.println("divisor: " + i);
+                num2++;
+            }
+        }
+        if (num2 == 2) {
+            System.out.println("es numero primo");
+
+        } else {
+            if (!(num2 == 2)) {
+                System.out.println("el numero no es primo");
+            }
+        }
+
+    }
+
+}
