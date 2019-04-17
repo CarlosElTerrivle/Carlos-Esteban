f56c453fd2d3a43c8c4271773395086eb5fc3a10
CarlosElTerrivle <48688160+CarlosElTerrivle@users.noreply.github.com>
2019-04-17 11:07:20 -0300

    Create arregloprimo.java

---
+++ arregloprimo.java
@@ -0,0 +1,42 @@
+public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rnum = new Random();
+        System.out.println("defina el largo del arreglo: ");
+        int a = in.nextInt();
+        int[] arreglo = new int[a];
+        int numpar = 0;
+        int numinpar = 0;
+        int numprimo = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+           /* arreglo[i] = rnum.nextInt(100);
+            //contar numeros pares
+//            System.err.println(arreglo[i]);
+             if (arreglo[i] % 2 == 0) {
+                numpar++;
+            } else {
+                numinpar++;
+            }
+             */
+        }
+        for (int b = 0; b < arreglo.length; b++) {
+            numprimo = 0;
+            for (int i = 1; i < 666; i++) {
+                if (arreglo[b] % i == 0) {
+                    numprimo++;
+                }
+            }
+//            System.out.println(numprimo);
+            System.out.println("el num: " + arreglo[b]);
+            if (numprimo == 2) {
+                System.out.println("es numero primo");
+            } else {
+                System.out.println("no es numero primo");
+            }
+        }
+//        System.out.println("numeros pares " + numpar);
+//        System.out.println("numeros impares " + numinpar);
+        System.out.println("numeros primos " + numprimo);
+
+    }
+
+}
